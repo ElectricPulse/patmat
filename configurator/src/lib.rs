b63a1d130dfeb4b4876d9dd9a1cb8fcd64a962b5
Adam Labus <alabus@i4industry.eu>
2026-08-18 20:54:18 +0200

    Use paragraph text style for Config tree items in Configurator

--- configurator/src/lib.rs
+++ configurator/src/lib.rs
@@ -167,7 +167,7 @@ impl Custom_widget_trait for Tree_menu_item {
         const INDENT: usize = 50;
         let theme = theme.affect(render).await?;
         let mut text = Text::new(&self.name);
-        let mut style = theme.specific.text.button;
+        let mut style = theme.specific.text.paragraph;
         if !selected {
             style.color = theme.semantic.text.muted;
         }
